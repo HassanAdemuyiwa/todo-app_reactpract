4f242e7ee82cf178e2c11e7e4b2b9f51a4babc01
hassanademuyiwa <ademuyiwahassan68@gmail.com>
2021-02-17 22:27:44 +0100

    todov1

--- src/component/Todos.js
+++ src/component/Todos.js
@@ -6,6 +6,7 @@ const todoList = todos.length ? (
     todos.map( todo => { 
         return( 
             <div className="collection-item" key={todo.id}>
+                <input type="checkbox"/>
                 <span onClick={()=> {deleteTodo(todo.id)}}>{todo.content}</span>
             </div>
         )
---
+++ src/todosData.js
@@ -0,0 +1,8 @@
+const todoData = [
+        {id: 1, content:"Go to Gym", complete: true},
+        {id: 2, content:"Do some laundry", complete: false},
+        {id: 3, content:"Feed the cat", complete: true}
+    ]
+
+export default todoData
+
